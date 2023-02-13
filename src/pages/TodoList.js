825bf0bf5950bf5c82b4b02e0bcca6a9521f6017
shings <shings@fittingmonster.com>
2023-02-13 17:05:50 +0900

    api주소 환경변수 설정

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,4 +1,14 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const TodoList = () => {
+  const baseUrl = process.env.REACT_APP_API_URL;
+  useEffect(() => {
+    axios.get(`${baseUrl}/todos`).then((response) => {
+      console.log(response);
+    }, []);
+  });
+
   return (
     <>
       <div>투두리스트</div>
